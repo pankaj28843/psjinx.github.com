f46fd2f69fcb08e46f6215d384ad3863344cf658
Pankaj Singh <psjinx@gmail.com>
2015-08-02 13:04:12 +0530

    Generated on Sun Aug  2 13:04:12 IST 2015

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-cd $(git rev-parse --show-toplevel);
-jekyll build --lsi --future --drafts;
-cd _site;
-git add -A $(git rev-parse --show-toplevel);
-git commit -am "Generated on `date`"
-git push;
-cd ..;
